5e2ca14bca2612c9e2f63b535208d330a3cd8040
VyvorThePaladin <38387282+VyvorThePaladin@users.noreply.github.com>
2018-10-15 21:58:12 -0400

    Add instruction to input provider

--- diagonal_difference.py
+++ diagonal_difference.py
@@ -1,6 +1,6 @@
 # Accepts n by n matrix from user and
 # computes the absolute diagonal difference
 
-# Enter value of n and then enter the matrix
+print('Enter value of n and then enter the matrix')
 print(abs(sum((lambda arr: int(arr[x]) - int(arr[len(arr) - x - 1]))
               (input().split()) for x in range(int(input())))))
