cfabae72264ab6766af1299b82bea6df3bcd7014
Sujit Konapur <satwork@Sujits-MacBook-Pro.local>
2019-02-18 00:19:56 -0500

    Collatz Sequence implementation

---
+++ collatz.py
@@ -0,0 +1,16 @@
+# The Collatz Sequence
+
+def collatz(number):
+
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+
+    elif number % 2 == 1:
+        result = 3 * number + 1
+        print(result)
+        return result
+
+x = input('Enter an integer please:\n')
+while x != 1:
+    x = collatz(int(x))
